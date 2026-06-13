9b290a3759b6272bf7ad90b84b7ea839ff8b967b
FabhianoG <fabiano.guerrero@protecso.com.pe>
2026-06-13 11:50:41 -0500

    feat: redirigir a la página de inicio a la vista de inicio de sesión

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,5 @@
-import BarraLateral from "./componentes/BarraLateral";
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <>
-
-      <main className="flex-1 flex flex-col h-screen overflow-y-auto bg-gray-50 p-4 space-y-6">
-        <BarraLateral />
-        <h1 className="text-2xl font-bold text-gray-800">Componentes UI para Colegios</h1>
-      </main>
-    </>
-  );
+  redirect('/login')
 }
